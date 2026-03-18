a4308841b0b1e3ea77283e219eecdf9eb38b397c
fortunelucky777 <waris980609@gmail.com>
2026-03-18 03:43:30 -0400

    fix: DAH-1865 - handle missing price_per_gpu gracefully in ExecutorInfo calculation

--- lium/sdk/client.py
+++ lium/sdk/client.py
@@ -135,7 +135,7 @@ class Lium:
                 if gpu_name:
                     gpu_type = extract_gpu_type(gpu_name)
 
-        price_per_gpu = executor_dict.get("price_per_gpu", 0)
+        price_per_gpu = executor_dict.get("price_per_gpu") or 0
         price_per_hour = price_per_gpu * gpu_count
 
         return ExecutorInfo(
